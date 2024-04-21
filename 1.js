e96e56cbac31e084aef56fe99116ba42ca0299bc
HumbalAli <160706785+HumbalAli@users.noreply.github.com>
2024-04-21 17:32:10 +0500

    Add files via upload
    it is my first project , It will require inquirer to work .

---
+++ 1.js
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+let awnser = await inquirer.prompt({
+    "name": "name",
+    "message": "what is your name",
+    "type": "input"
+});
+console.log(`hello ${awnser.name}`);
+let dish = await inquirer.prompt({
+    "name": "dish",
+    "message": "what would you like to eat",
+    "type": "list",
+    choices: [
+        "Spaghetti Carbonara",
+        "Chicken Tikka Masala",
+        "Sushi Rolls",
+        "Hamburger",
+        "Pad Thai",
+        "Lasagna",
+        "Miso Soup",
+        "Margherita Pizza",
+        "Steak Frites",
+        "Caesar Salad"
+    ]
+});
+console.log(`Certainly I will bring your dish ${dish.dish}`);
